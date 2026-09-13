ed2f89e35a7e74b3b7f6eaf5d0e7bd683bdfe0c1
cucina656-dotcom <cucina656@gmail.com>
2026-09-13 03:28:30 -0700

    home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2723,13 +2723,12 @@ function Home() {
       : activeCategory === "tv"
         ? { label: "Share your moment", postType: "moment" }
         : { label: "Offer work or trade skills", postType: "offer" };
-  let mainContent;
-  if (activeCategory === "connect") {
-    mainContent = (
-      <ConnectExperience
-        user={user}
-        isLoggedIn={isLoggedIn}
-        onRequireAuth={requireAuthForConnect}
+let mainContent;
+if (activeCategory === "social-life") {
+  mainContent = <SocialLifeLanding />;
+} else if (activeCategory === "connect") {
+  mainContent = (
+    <ConnectExperience
       />
     );
   } else if (loading) {
